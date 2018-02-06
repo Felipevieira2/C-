5a61b410ab3254828a3ddb649a58812cd745d626
Felipevieira2 <felipy_erivaldo@hotmail.com>
2018-02-05 22:15:40 -0200

    Add files via upload

---
+++ CadastroDeArtistas/CadastroDeArtistas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CadastroDeArtistas
+{
+    class Program
+    { 
+        public static List<Artista> Artistas = new List<Artista>();
+        static void Main(string[] args)
+        {
+            
+            Artistas.Add(new Artista(101, "Scarlett Johansson", 4000000.00));
+            Artistas.Add(new Artista(102, "Chris Evans", 2500000.00));
+            Artistas.Add(new Artista(103, "Robert Downey Jr.", 3000000.00));
+            Artistas.Add(new Artista(104, "Morgan Freeman", 4000000.00));
+            Artistas.Sort();
+            int opcao = 0;
+            while (opcao != 5)
+            {
+                Console.Clear();
+                Tela.MostrarTela();
+                Console.WriteLine("Escolha uma opção!!!");
+                opcao = int.Parse(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        Tela.ListarProduto();
+                        break;
+                    case 2:
+                        Tela.CadastrarProduto();
+                        Console.ReadKey();
+                        break;
+                    
+                    
+                }
+
+            }
+          
+        }
+    }
+}
---
+++ CadastroDeArtistas/CadastroDeArtistas/Tela.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CadastroDeArtistas
+{
+    class Tela
+    {
+       public static void MostrarTela()
+        {
+            Console.WriteLine("1 - Artistas ordenadamente");
+            Console.WriteLine("2 - Cadastrar artista");
+            Console.WriteLine("3 - Cadastrar filme");
+            Console.WriteLine("4 - Mostrar dados de um filme");
+            Console.WriteLine("5 - Sair ");
+        }
+
+        public static void ListarProduto()
+        {
+            for(int i = 0; i<Program.Artistas.Count; i++)
+            {
+                Console.WriteLine(Program.Artistas[i]);
+            }
+            Console.ReadLine();
+        }
+
+        public static void CadastrarProduto()
+        {
+            Console.Write("Digite o código do novo Artista: ");
+            int codProduto = int.Parse(Console.ReadLine());
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o valor do Cachê");
+            double preco = double.Parse(Console.ReadLine(Inva));
+            Artista artista = new Artista(codProduto, nome, preco);
+            Program.Artistas.Add(artista);
+            Console.WriteLine("Cadastrado com sucesso!!!");
+            Console.ReadLine();
+        }
+    }
+}
